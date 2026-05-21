2e3deb1060f2f59e5a4105e697bcab9de8f62673
Rishika <rishikarpatil710@gmail.com>
2026-05-21 21:37:55 +0530

    fixing backend url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: " https://pharmacy-inventory-system-a1mw.onrender.com/api"
+    baseURL: "https://pharmacy-inventory-system-a1mw.onrender.com/api"
 });
 
 
