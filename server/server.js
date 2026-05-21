ab9bed86dd27715859fa809cdcabd111cd5e8713
Rishika <rishikarpatil710@gmail.com>
2026-05-21 13:09:56 +0530

    fixing backend url

--- server/server.js
+++ server/server.js
@@ -57,7 +57,10 @@ app.use(cors({
 
 app.use(express.json());
 
-
+app.use((req, res, next) => {
+    console.log(req.method, req.url);
+    next();
+});
 // Routes
 const authRoutes =
     require("./routes/authRoutes");
@@ -96,6 +99,7 @@ app.use(
     "/api/notifications",
     notificationRoutes
 );
+
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
