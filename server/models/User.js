612a6cf937b231cb4e4833c37a80e57c98013bdc
Rishika <rishikarpatil710@gmail.com>
2026-05-21 22:06:53 +0530

    Changing USER.JS

--- server/models/User.js
+++ server/models/User.js
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema(
     role: {
         type: String,
         enum: ["admin", "user" , "store_manager", "warehouse_manager"],
-        default: "store_manager"
+        default: "user"
     }
 },
 {
